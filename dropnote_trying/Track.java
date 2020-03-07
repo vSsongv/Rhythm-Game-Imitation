6f48aa544284aa0f46ab1573c6f6437f47b81a3a
vSsongv <songdh21@gmail.com>
2020-03-07 16:12:50 +0900

    Try to note drop

---
+++ dropnote_trying/Track.java
@@ -0,0 +1,59 @@
+package dynamic_beat;
+
+public class Track {
+	private String titleImage; //Title name image
+	private String startImage; //Image before game start
+	private String gameImage; //Image during play
+	private String startMusic; //Hook part of music
+	private String gameMusic; //Play music
+	private String titleName; //title name
+	
+	public String getTitleImage() {
+		return titleImage;
+	}
+	public void setTitleImage(String titleImage) {
+		this.titleImage = titleImage;
+	}
+	public String getStartImage() {
+		return startImage;
+	}
+	public void setStartImage(String startImage) {
+		this.startImage = startImage;
+	}
+	public String getGameImage() {
+		return gameImage;
+	}
+	public void setGameImage(String gameImage) {
+		this.gameImage = gameImage;
+	}
+	public String getStartMusic() {
+		return startMusic;
+	}
+	public void setStartMusic(String startMusic) {
+		this.startMusic = startMusic;
+	}
+	public String getGameMusic() {
+		return gameMusic;
+	}
+	public void setGameMusic(String gameMusic) {
+		this.gameMusic = gameMusic;
+	}
+	public String gettitleName() {
+		return titleName;
+	}
+	public void settitleName(String gameMusic) {
+		this.titleName = titleName;
+	}
+	
+	public Track(String titleImage, String startImage, String gameImage, String startMusic, String gameMusic, String titleName) {
+		super();
+		this.titleImage = titleImage;
+		this.startImage = startImage;
+		this.gameImage = gameImage;
+		this.startMusic = startMusic;
+		this.gameMusic = gameMusic;
+		this.titleName = titleName;
+	}
+	
+	
+}
